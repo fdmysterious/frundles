3186ce34fc790f779d03ff334e823328e4f62681
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2024-08-24 13:16:20 +0200

    Initial :dancer:

---
+++ src/frundles/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2024-present Florian Dupeyron <florian.dupeyron@mugcat.fr>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+"""
+# Frundles
+
+- Florian Dupeyron <florian.dupeyron@mugcat.fr>
+- August 2024
+
+IP cataolog manager
+"""
