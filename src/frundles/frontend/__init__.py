4a0deae44f3e2cd5575016a46aaca2d6babe5b47
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2024-10-10 15:33:49 +0200

    Add --output_mode argument to select active output handler

--- src/frundles/frontend/__init__.py
+++ src/frundles/frontend/__init__.py
@@ -13,6 +13,7 @@ from . import locate
 from . import list as cmd_list
 
 
+from frundles.io.available_handlers import AVAILABLE_HANDLERS, default as default_handler
 from frundles.io.base import OutputHandlerLogging
 from frundles.io.tty import TTYOutputHandler
 
@@ -34,6 +35,10 @@ def main():
         description="Frundles is a simple HDL library manager",
         epilog="Hmmmm... I'm Mr.Frundles!",
     )
+
+    # Add argument for output handler
+    parser.add_argument("--output_mode", choices=AVAILABLE_HANDLERS.keys(), default=default_handler(), help="Set output mode for integration with other tools")
+
     subcommand = parser.add_subparsers(dest="subcommand")
 
     for cmd in CLI_COMMANDS.values():
@@ -47,7 +52,7 @@ def main():
         sys.exit(1)
     else:
         # Setup output handler
-        output_handler = TTYOutputHandler()
+        output_handler = AVAILABLE_HANDLERS[args.output_mode]()
         output_handler.configure()
 
         log_handler = OutputHandlerLogging(output_handler)
--- src/frundles/io/available_handlers.py
+++ src/frundles/io/available_handlers.py
@@ -6,9 +6,17 @@
 """
 
 from .vivado import VivadoOutputHandler
+from .tty    import TTYOutputHandler
+
+AVAILABLE_HANDLERS = {
+    "tty": TTYOutputHandler,
+    "vivado": VivadoOutputHandler
+}
 
 
 def get(name: str):
-    AVAILABLE_HANDLERS = {"vivado": VivadoOutputHandler}
-
     return AVAILABLE_HANDLERS.get(name, None)
+
+def default():
+    return "tty"
+
